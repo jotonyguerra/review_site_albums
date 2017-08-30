3d593c392ca337d7bd341fd2acca9cd2fa4f01a6
Jotony Guerra <jotonyguerra@gmail.com>
2017-08-30 13:03:52 -0700

    adding factory_girl

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'albums#index'
-  resources
+  # root 'albums#index'
+  # resources
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
