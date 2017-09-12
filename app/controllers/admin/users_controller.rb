1dc95de77c7c3351665e3cee8b64f5cc149fda1e
Jotony Guerra <jotonyguerra@gmail.com>
2017-09-12 14:58:16 -0700

    broke then fixed admin features

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,7 +12,7 @@ class Admin::UsersController < ApplicationController
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
-    redirect_to users_path
+    redirect_to admin_users_path
   end
 
   private
