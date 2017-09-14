74004cd5d48503f5ee5d980d7dd9e8b5ef1e0b5c
Jotony Guerra <jotonyguerra@gmail.com>
2017-09-14 13:05:25 -0700

    fixed views for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,7 +13,35 @@ class ReviewsController < ApplicationController
     redirect_to @album
   end
 
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.review = params[:review][:review]
+    @album.rating = params[:review][:rating]
+    if @review.review.nil? || @review.rating.nil?
+      flash[:errors] = "review and/or rating cannot be empty "
+      render 'edit'
+    else
+      @review.save
+      flash[:success] = "review updated"
+      redirect_to album_path(@album)
+    end
+  end
+
+  def destroy
+    Review.find(params[:id]).destroy
+    flash[:success] = "Review Deleted"
+    redirect_to album_path(@album)
+  end
+
   private
+  def set_album
+    @album = Album.find(review_params[:album_id])
+  end
+  
   def review_params
     params.require(:review).permit(:rating, :review, :album_id)
   end
